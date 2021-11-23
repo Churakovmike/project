af5a08d2b58fc16551cf2c28b0d1be76e23f7bee
m.churakov <churakovmike@mail.ru>
2021-11-24 00:44:48 +0700

    *

---
+++ src/TestClass.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ChurakovMike\Project;
+
+class TestClass
+{
+    public function func1(): string
+    {
+        return 'func1';
+    }
+
+    public function func2(): int
+    {
+        return 222;
+    }
+
+    public function func3(): array
+    {
+        return [];
+    }
+}
